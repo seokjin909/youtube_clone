3792628a4772e53c1e8e90bdfaf1fd68e4875681
jin <thseokjin@naver.com>
2023-09-08 00:59:22 +0900

    스타일링 처리

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -7,15 +7,14 @@ import { useYoutubeApi } from "../context/YoutubeApiContext";
 export default function Videos() {
   const { keyword } = useParams();
   const { youtube } = useYoutubeApi();
-  const { isLoading, error, data: videos } = useQuery(["videos", keyword], () => youtube.search(keyword));
+  const { isLoading, error, data: videos } = useQuery(["videos", keyword], () => youtube.search(keyword), { staleTime: 1000 * 60 * 1 });
 
   return (
     <>
-      <div>Videos {keyword ? `🔍${keyword}` : "🔥"}</div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 🫠</p>}
       {videos && (
-        <ul>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
